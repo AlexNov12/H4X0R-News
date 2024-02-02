94b326f347316e57044add97396cd473a4e7b894
AlexNov12 <nov.alex96@gmail.com>
2024-02-02 05:42:39 +0300

    first commit

---
+++ H4X0R News/Models/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  H4X0R News
+//
+//  Created by Александр Новиков on 31.01.2024.
+//
+
+import Foundation
---
+++ H4X0R News/Models/PostData.swift
@@ -0,0 +1,8 @@
+//
+//  PostData.swift
+//  H4X0R News
+//
+//  Created by Александр Новиков on 31.01.2024.
+//
+
+import Foundation
