ae4515c9f1f96c95261e3f0fb0cd552a5aa5a3e4
AlexNov12 <nov.alex96@gmail.com>
2024-02-03 11:18:40 +0300

    Second commit

---
+++ H4X0R News/Views/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Александр Новиков on 31.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
+                
+            }
+            .navigationTitle("H4X0R NEWS")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Bonjour"),
+//    Post(id: "3", title: "Hola")
+//]
---
+++ H4X0R News/Views/WebView.swift
@@ -0,0 +1,8 @@
+//
+//  WebView.swift
+//  H4X0R News
+//
+//  Created by Александр Новиков on 03.02.2024.
+//
+
+import Foundation
